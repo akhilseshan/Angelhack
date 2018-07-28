b65f56e3c71d9dd8fb6e1b4b0ff16a3d4faab973
Shahul Es <shahules786@gmail.com>
2018-07-28 23:20:20 +0530

    Update justgo.py

--- justgo.py
+++ justgo.py
@@ -2,6 +2,6 @@ from django.db import connection
 
 def my_custom_sql(self):
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM accounts_account where id=%s",[self.id])
+    cursor.execute("SELECT * FROM bus_details where from_=%s",[self.id])
     row = cursor.fetchall()
     return row
